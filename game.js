e6a10d32ab39a7acf34f0d83753f1a0e799fc270
Gabriel Erbetta <gabrielerbetta@gmail.com>
2020-09-06 13:23:19 -0300

    Improved visuals and gameplay

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 const MAX_W = 1200;
 const MAX_H = 800;
 
+const LIGHT_BLUE = "#228fe3";
+const PURPLE = "#3d51ab";
+const AQUAMARINE = "#3ddc97";
+const BURGUNDY = "#6c0e23";
+const GRULLO = "#A69888";
+const DARK_GREY = "#1c1b1b";
+const GREY = "#666";
+
 const PLACES = [
   { id: 0, name: 'Salvador', x: 124, y: 118 },
   { id: 1, name: 'Rio de Janeiro', x: 95, y: 160 },
@@ -38,6 +46,7 @@ let map = Sprite({
   radius: (MAX_H / 2) - 100,
   centerX: 0,
   centerY: 0,
+  // interactive: true,
 
   zoomIn: function() {
     if (this.scaleX == 2) return;
@@ -69,7 +78,7 @@ let map = Sprite({
   },
 
   collidesWithPointer: function(pointer) {
-    return true;
+    //return true;
     let dx = pointer.x - this.x;
     let dy = pointer.y - this.y;
     return Math.sqrt(dx * dx + dy * dy) < this.radius;
@@ -78,6 +87,7 @@ let map = Sprite({
   onDown: function(pointer) {
     // handle on down events on the sprite
     console.log(pointer.x, pointer.y)
+    console.log('raio',this.radius)
     let x = pointer.x - this.x - canvas.offsetLeft;
     let y = pointer.y - this.y - canvas.offsetTop;
 
@@ -89,10 +99,14 @@ let map = Sprite({
 
     console.log(`{ id: 1, name: '', x: ${x}, y: ${y} } // ${this.centerX}`);
 
+    // if (this.interactive) createJump(x, y);
     createJump(x, y);
   },
 
-  update: function() {
+  move: function(x, y) {
+    this.centerX += x;
+    this.centerY += y;
+
     this.centerY = clamp(-250, 250, this.centerY);
     this.y = this.initialY + this.centerY;
 
@@ -102,6 +116,14 @@ let map = Sprite({
     if (this.centerX < 0) {
       this.centerX += this.radius * 4;
     }
+
+    this.children.forEach((c) => {
+      c.update();
+    });
+  },
+
+  update: function() {
+    return;
   },
 
   render: function () {
@@ -160,12 +182,38 @@ let map = Sprite({
     this.context.closePath();
     this.context.stroke();
     this.context.restore();
+
+    // selectedJumps.forEach((e, i) => {
+    //   if (i == 0 || i == currentJump) return;
+    //
+    //   let x0 = selectedJumps[i - 1].x;
+    //   let y0 = selectedJumps[i - 1].y;
+    //
+    //   let x1, y1;
+    //   if (!this.interactive) {
+    //     x1 = x0 + (e.x - x0) * (e.progress / 100);
+    //     y1 = y0 + (e.y - y0) * (e.progress / 100);
+    //   } else {
+    //     x1 = e.x;
+    //     y1 = e.y;
+    //   }
+    //
+    //   this.context.beginPath();
+    //   this.context.moveTo(x0, y0);
+    //   this.context.setLineDash([5, 10]);
+    //   this.context.lineWidth = 2;
+    //   this.context.lineTo(x1, y1);
+    //   this.context.closePath();
+    //   this.context.stroke();
+    // });
   }
 });
 
 
-// JUMPS
+// JUMPS GRID
 let jumpsGrid = Grid({
+  x: 12,
+  y: 12,
   anchor: {x: 0, y: 0},
   rowGap: 20,
 });
@@ -248,7 +296,7 @@ load('./arrow.png', './arrow_small.png', './zoomin.png', './zoomout.png').then((
     ...buttonCommon,
 
     update: function() {
-      if (this.pressed || keyPressed(this.key)) map.centerX -= 4;
+      if (this.pressed || keyPressed(this.key)) map.move(-4, 0);
     }
   });
 
@@ -262,7 +310,7 @@ load('./arrow.png', './arrow_small.png', './zoomin.png', './zoomout.png').then((
     ...buttonCommon,
 
     update: function() {
-      if (this.pressed || keyPressed(this.key)) map.centerX += 4;
+      if (this.pressed || keyPressed(this.key)) map.move(4, 0);
     }
   });
 
@@ -276,7 +324,7 @@ load('./arrow.png', './arrow_small.png', './zoomin.png', './zoomout.png').then((
     ...buttonCommon,
 
     update: function() {
-      if (!this.disabled && (this.pressed || keyPressed(this.key))) map.centerY += 4;
+      if (!this.disabled && (this.pressed || keyPressed(this.key))) map.move(0, 4);
     }
   });
 
@@ -291,7 +339,7 @@ load('./arrow.png', './arrow_small.png', './zoomin.png', './zoomout.png').then((
     ...buttonCommon,
 
     update: function() {
-      if (!this.disabled && (this.pressed || keyPressed(this.key))) map.centerY -= 4;
+      if (!this.disabled && (this.pressed || keyPressed(this.key))) map.move(0, -4);
     }
   });
 
@@ -309,21 +357,29 @@ let sidebar = Sprite({
   y: 0,
   width: 300,
   height: MAX_H,
-  color: 'darkslategrey',
-  children: [jumpsGrid]
+  color: DARK_GREY,
+  children: [jumpsGrid],
+  render: function() {
+    this.context.strokeStyle = GREY;
+    this.context.lineWidth = 4;
+    this.draw();
+    this.context.strokeRect(2, 2, this.width - 4, this.height - 4);
+  }
 });
 
 // NEXT JUMP
 let nextBt = Button({
-  x: MAX_W - 320,
-  y: MAX_H - 20,
+  x: MAX_W - 300 - 120,
+  y: MAX_H - 50,
+  padX: 20,
+  padY: 20,
   disabled: true,
-  anchor: {x: 1, y: 1},
+  anchor: {x: 0.5, y: 0.5},
   text: {
     text: 'NEXT JUMP',
-    color: '#ddd',
-    font: '30px Arial, sans-serif',
-    anchor: {x: 1, y: 1},
+    color: PURPLE,
+    font: 'bold 30px monospace',
+    anchor: {x: 0.5, y: 0.5}
   },
   update: function() {
     this.disabled = selectedJumps.length <= currentJump;
@@ -331,20 +387,34 @@ let nextBt = Button({
   render: function() {
     if (this.disabled) {
       this.textNode.text = '';
+      return;
     } else {
       this.textNode.text = 'NEXT JUMP';
     }
 
     if (this.hovered) {
       isPointer = true;
-      this.textNode.color = '#ff0';
-    } else {
-      this.textNode.color = '#ddd';
     }
+
+    this.context.lineWidth = 8;
+    this.context.strokeStyle = PURPLE;
+    this.context.fillStyle = LIGHT_BLUE;
+    this.context.strokeRect(0, 0, this.width, this.height);
+    this.context.fillRect(0, 0, this.width, this.height);
   },
   onDown: function() {
-    nextJump()
+    nextJump();
   }
+});
+
+// POINTS
+let pointsCounter = Sprite({
+  anchor: {x: 0, y: 1},
+  x: 0,
+  y: MAX_H,
+  width: 200,
+  height: 40,
+  color: 'darkslategrey'
 });
 
 
@@ -380,6 +450,7 @@ let startBt = Button({
   },
   ...menuButtonCommon,
   onDown: function() {
+    generateJumps();
     menuScene.hide();
     gameScene.show();
   },
@@ -412,7 +483,7 @@ let menuScene = Scene({
 // MAIN GAME
 let gameScene = Scene({
   id: 'game',
-  children: [map, sidebar, nextBt]
+  children: [map, sidebar, pointsCounter, nextBt]
 });
 
 
@@ -476,8 +547,10 @@ function generateJumps() {
       width: 300,
       jumpNumber: i,
       render: function() {
-        if (currentJump == this.jumpNumber) {
-          this.color = 'yellow';
+        if (currentJump > this.jumpNumber) {
+          this.color = AQUAMARINE;
+        } else if (currentJump == this.jumpNumber) {
+          this.color = PURPLE;
         } else {
           this.color = 'white';
         }
@@ -499,22 +572,50 @@ function createJump(x, y) {
     initialY: y,
     x: x,
     y: y,
-    color: 'yellow',
+    color: AQUAMARINE,
+    // progress: currentJump == 0 ? 100 : 0,
 
     update: function() {
       this.x = x - map.centerX;
       if (this.x < - map.radius) this.x += map.radius * 4;
 
-      this.color = this.jumpNumber == currentJump ? 'purple' : 'yellow';
+      this.color = this.jumpNumber == currentJump ? PURPLE : AQUAMARINE;
+
+      // if (this.jumpNumber != currentJump - 1) return;
+      // if (this.progress != 100) {
+      //   this.progress++;
+      // } else {
+      //   checkJump();
+      // }
     },
 
     render: function() {
       this.context.save();
+
       this.context.fillStyle = this.color;
+      this.context.strokeStyle = DARK_GREY;
+      this.context.lineWidth = 1;
+
       this.context.beginPath();
       this.context.arc(0, 0, 3, 0, 2 * Math.PI);
       this.context.closePath();
       this.context.fill();
+      this.context.stroke();
+
+      this.context.beginPath();
+      this.context.moveTo(0,0);
+      this.context.bezierCurveTo(2,-10,-20,-25,0,-30);
+      this.context.bezierCurveTo(20,-25,-2,-10,0,0);
+      this.context.closePath();
+      this.context.fill();
+      this.context.stroke();
+
+      this.context.beginPath();
+      this.context.arc(0,-21,3,0,Math.PI*2);
+      this.context.closePath();
+      this.context.fillStyle = 'white';
+      this.context.fill();
+
       this.context.restore();
     }
   });
@@ -524,19 +625,29 @@ function createJump(x, y) {
 }
 
 function nextJump() {
+  let i = currentJump;
+  let jump = selectedJumps[i];
+  let distance = Math.sqrt((jump.initialX - jumps[i].x)**2 + (jump.initialY - jumps[i].y)**2);
+  if (distance > 1200) distance -= 1200;
+  if (distance > 10) {
+    alert(` 404 \n YOU LOSE \n DISTANCE ${distance} `);
+    location.reload();
+    return;
+  }
+
+  // map.interactive = true
   currentJump++;
   if (currentJump == 5) return finishGame();
 }
 
 function finishGame() {
-  let message = '';
+  let message = 'YOU WIN!\n';
 
   selectedJumps.forEach((j, i) => {
     let distance = Math.sqrt((j.initialX - jumps[i].x)**2 + (j.initialY - jumps[i].y)**2);
-    if (distance > 10) message += `MISSED ${jumps[i].name} BY ${distance}\n`;
+    if (distance > 1200) distance -= 1200;
+    message += `${jumps[i].name} WITH DISTANCE ${distance}\n`;
   });
-
-  if (message == '') message = 'YOU WIN!';
 
   alert(message);
   location.reload();
@@ -570,7 +681,14 @@ let loop = GameLoop({
 });
 
 track(map);
+
+// OPEN MENU
+//gameScene.hide();
+//menuScene.show();
+
+// STRAIGHT TO GAME
 generateJumps();
-gameScene.hide();
-menuScene.show();
+menuScene.hide();
+gameScene.show();
+
 loop.start(); // start the game
